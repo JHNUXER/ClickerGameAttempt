0509c54fefa981628c3d1003c7f8eea360ac2e46
John Kayne <johnhoganuke2@gmail.com>
2017-08-18 19:43:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ Hardware.prototype.onInstall = function() {
   return;
 }
 
+class Drive extends Hardware {
+  constructor(sName,nCPUConsumption,nStorageBytes) {
+    super(sName,nCPUConsumption);
+    this.totalStorage = nStorageBytes;
+    this.avaliableStorage = nStorageBytes;
+  }
+}
+
 // Init:
 Game = {};
 Game.version = "v0.0.1ppa";
+Game.Hardware = [
+  new Drive("Basic Hard Drive",4,1000)
+]
 
 Computer.installHardware = function(x) {
   Computer.hardware.push(x);
